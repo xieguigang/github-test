f1cdf5d711e9634514e7d24209fb4e7935de0788
xieguigang <xie.guigang@gmail.com>
2016-05-28 23:48:05 +0800

    add      program

---
+++ github-test/Program.vb
@@ -0,0 +1,16 @@
+﻿Module Program
+
+    Const Github As String = "https://github.com/"
+
+    Sub Main()
+        Do While True
+            Try
+                Dim respo As String = Github.GetRequest(https:=True)
+                Call Process.Start(Github)
+                Return
+            Catch ex As Exception
+                Call ex.PrintException
+            End Try
+        Loop
+    End Sub
+End Module
